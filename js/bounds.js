cdf4e53cdeb1cf001303d9febbc9c4cc975e5be1
David <87504880+darud63@users.noreply.github.com>
2023-08-07 13:17:30 +0300

    Update bounds.js

--- js/bounds.js
+++ js/bounds.js
@@ -1,4 +1,5 @@
 var kingdoms={
 	type:"FeatureCollection",features:[
 	{ type:"Feature",properties:{bg:"#F85559"},geometry:{type:"MultiPolygon",coordinates:[[[[5892,2692],[5892,2575],[6043,2575],[6043,2692]]]]}},
+        {type:"Feature",properties:{bg:"#414147",name:"Хаб"},geometry:{type:"MultiPolygon",coordinates:[[[[1010,1010],[1010,1038],[1038,1038],[1038,1010]]]]}},
 	{type:"Feature",properties:{bg:"#5C52CF",name:"Банк"},geometry:{type:"MultiPolygon",coordinates:[[[[6540,1445],[6248,1445],[6248,1710],[6540,1710]]]]}}]};
